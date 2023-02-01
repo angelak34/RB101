d6debbb1f5bb9833475bf6e669eed4b209944b98
Angela Kim <angelahjk25@gmail.com>
2023-01-31 20:41:12 -0500

    practice

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -11,7 +11,7 @@ number1 = Kernel.gets().chomp()
 Kernel.puts("What's the second number? ")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide ")
+Kernel.puts("What operation would you like to perform? 1: add 2: subtract 3: multiply 4: divide ")
 operator = Kernel.gets().chomp()
 
 if operator == '1'
